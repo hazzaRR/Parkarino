7330746cf49d09f5161d5f171844f9002893625a
zakb27 <zakbrook02@gmail.com>
2022-03-20 18:08:11 +0000

    Temp

--- Account/account.js
+++ Account/account.js
@@ -1,1 +1,14 @@
 //account.js
+class Account {
+    id;
+    userName;
+    email;
+    password;
+    firstName;
+    lastName;
+    userType;
+
+    constructor(email, password, firstName, lastName, userType) {
+        this.
+    }
+}
