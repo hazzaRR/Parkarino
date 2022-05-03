0c4672c2de498d39e8f4b7d13b0c816a30be4dde
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2022-05-03 18:37:00 +0100

    fetch carpark options and add them to the request select options

--- public/Driver/makeRequest.js
+++ public/Driver/makeRequest.js
@@ -35,6 +35,35 @@ async function makeRequest(event) {
 
 }
 
+//fetches all the carparks in the database and adds them to the select box for the driver to pick from
+async function getCarparks() {
+    const allCarparks_res = await fetch('/view-carpark/all-carparks');
+    const carparks_json = await allCarparks_res.json();
+
+    carparks_str = carparks_json.replace(/[^a-zA-Z, ]/g, ""); // strip all specials except comma,spaces
+    const options = carparks_str.split(",");
+    //console.log(carparks_str);
+    
+    if (allCarparks_res.status == 200)
+    {
+        options.forEach(item => addOptions(item)); // add new select option for each car park
+    }
+    else {
+        document.getElementById('viewCarparkForm').innerHTML = "~ apparently there are no car parks available :( ~";
+    }
+}
+
+//adds the carpark options to the select tag 
+function addOptions(item)
+{
+    const ops = document.getElementById("location");
+    const option = document.createElement("option");
+    option.text = item;
+    ops.add(option);
+}
+
+document.addEventListener('DOMContentLoaded', getCarparks);
+
 
 function getMaxDepartureDate() {
 
